bc9221b59e750e0390d4881b23fbe883fbab82d6
AdamZadorski97 <adam.zadorski97@gmail.com>
2021-11-20 19:20:20 +0100

    Added TextMeshPro Asset Added Main Menu Scene

---
+++ Assets/Main/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void OnButtonStartClick()
+    {
+
+    }
+    public void OnButtonOptionsClick()
+    {
+
+    }
+    public void OnButtonExitClick()
+    {
+        Application.Quit();
+    }
+}
